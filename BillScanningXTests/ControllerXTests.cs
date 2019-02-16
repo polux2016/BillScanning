a437d697d44052b5a1b5936241b0d869c2839ee7
polux2016 <poluhovichandrey@gmail.com>
2019-02-16 10:45:22 +0200

    XUnut with examples added

---
+++ BillScanningXTests/ControllerXTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using BillScanning.Controllers;
+
+namespace BillScanningXTests
+{
+    public class ControllerXTests
+    {
+        [Fact]
+        public void TestFact()
+        {
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void TestTheory(int id)
+        {
+
+        }
+    }
+}
