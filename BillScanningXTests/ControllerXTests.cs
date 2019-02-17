cb8d82c7542d6421604b62676e74aee97ca5915c
Yaroslav Venzhyk <yaroslav.venzhyk@wikrgroup.com>
2019-02-17 21:36:49 +0200

    Mock package + simple test

--- BillScanningXTests/ControllerXTests.cs
+++ BillScanningXTests/ControllerXTests.cs
@@ -1,6 +1,10 @@
-using System;
-using Xunit;
+using System.Collections.Generic;
+using System.Linq;
 using BillScanning.Controllers;
+using BillScanningController.Abstract;
+using BillScanningController.Enteties;
+using Moq;
+using Xunit;
 
 namespace BillScanningXTests
 {
@@ -9,7 +13,22 @@ namespace BillScanningXTests
         [Fact]
         public void TestFact()
         {
+            // Arrange
+            Mock<IValueRepository> mock = new Mock<IValueRepository>();
+            mock.Setup(m => m.Values).Returns(new List<Value>()
+            {
+                new Value{ ValueId = 1, IsPositive = true, Digit = 3 },
+                new Value{ ValueId = 2, IsPositive = false, Digit = 5 },
+                new Value{ ValueId = 3, IsPositive = true, Digit = 9 },
+            });
+            ValuesController controller = new ValuesController(mock.Object);
 
+            //Act
+            List<Value> result = controller.ShowSimilarValues(true);
+
+            //Assert
+            Assert.True(result.Count == 2);
+            Assert.True(result.Where(v => v.IsPositive == false).ToArray().Length == 0);
         }
 
         [Theory]
